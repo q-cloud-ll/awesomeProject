987b4aa6fc90ac8026f110e4a791bdf95cb8a1d1
cherry <cherryqcsj@gmail.com>
2024-02-29 08:59:05 +0100

    fix v2

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "[v1] Hello, Kubernetes!\n")
+	io.WriteString(w, "[v2] Hello, Kubernetes!\n")
 }
 
 func main() {
