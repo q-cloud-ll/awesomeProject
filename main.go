9a3864c99b4cf3f1735ba422fcb973b045b4524e
cherry <cherryqcsj@gmail.com>
2024-03-01 03:45:05 +0100

    cc

--- main.go
+++ main.go
@@ -13,3 +13,16 @@ func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":3000", nil)
 }
+
+//func main() {
+//	wg := sync.WaitGroup{}
+//	for i := 0; i < 5; i++ {
+//		wg.Add(1)
+//		go func(wg *sync.WaitGroup, i int) {
+//			fmt.Printf("i:%d", i)
+//			wg.Done()
+//		}(&wg, i)
+//	}
+//	wg.Wait()
+//	fmt.Println("exit")
+//}
